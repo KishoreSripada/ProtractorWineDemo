5f528544d1725160929ed7833cea8d524df936e3
Kishore Sripada <sripadaskishore@gmail.com>
2018-04-13 01:09:47 +0800

    Updated to run cucumber feature file

--- conf.js
+++ conf.js
@@ -1,7 +1,17 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['spec.js'],
+  specs: ['features/addwine.feature'],
   capabilities: {
       browserName:'chrome'
-  }
+  },
+    cucumberOpts: {
+        strict:true,
+        require: [
+            'features/step_definitions/*.js',
+            'features/step_definitions/support/*.js'],
+        tags: true,
+        format: "./reports/cucumber-test-results.json",
+        profile: false,
+        'no-source': true
+    }
 };
